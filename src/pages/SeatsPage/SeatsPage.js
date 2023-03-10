f55e606ede46fa7387ca306225e39b63fbdb7220
Victor Mafra <victorvmafra97@gmail.com>
2023-03-10 15:23:50 -0300

    Selection seats version

--- src/pages/SeatsPage/SeatsPage.js
+++ src/pages/SeatsPage/SeatsPage.js
@@ -1,21 +1,46 @@
 import { useEffect, useState } from "react"
-import { useParams, Link } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 import axios from "axios"
 import styled from "styled-components"
 
 export default function SeatsPage() {
     const [seatsMovie, setSeatsMovie] = useState(undefined)
     const [selectedSeat, setSelectedSeat] = useState([])
+    const [movieSession, setMovieSession] = useState(undefined)
+    const [nameId, setName] = useState("")
+    const [cpf, setCpf] = useState("")
     const { idSessao } = useParams()
-    
-    
+    const navigate = useNavigate()
+
+
     console.log(selectedSeat)
-    
-    function clickSeat(seat){
-        console.log(seat.isAvailable)
-        const newSeat = [...selectedSeat]
-        setSelectedSeat([...newSeat, seat])
+
+    function clickSeat(seat, available) {
+
+        if (!available) {
+            alert("Esse assento não está disponível")
+        }
+        else {
+            setSelectedSeat((prevSeats) => {
+                if (prevSeats.includes(seat)) {
+                    return prevSeats.filter((prevSeats) => prevSeats !== seat);
+                }
+                else {
+                    return [...prevSeats, seat];
+                }
+            })
+        }
     }
+
+    function addSeats(e) {
+        e.preventDefault()
+        const urlPost = "https://mock-api.driven.com.br/api/v8/cineflex/seats/book-many"
+        const body = { ids: selectedSeat, name: nameId, cpf }
+    
+        const promise = axios.post(urlPost, body)
+        promise.then(res => navigate("/sucesso"))
+        promise.catch(err => alert(err.response.data))
+      }
 
 
     useEffect(() => {
@@ -23,7 +48,8 @@ export default function SeatsPage() {
         const promise = axios.get(urlSeats)
 
         promise.then((res) => {
-            setSeatsMovie(res.data)
+            setSeatsMovie(res.data.seats)
+            setMovieSession(res.data)
             console.log(res.data)
         })
 
@@ -39,52 +65,62 @@ export default function SeatsPage() {
     return (
         <PageContainer>
             Selecione o(s) assento(s)
-            
+
             <SeatsContainer>
-            {seatsMovie.seats.map((sea) => (
-                <SeatItem 
-                key={sea.id}
-                id={sea.id}
-                isAvailable={sea.isAvailable} 
-                onClick={() => clickSeat(sea.name, sea.isAvailable)}>
-                    {sea.name}
+                {seatsMovie.map((sea) => (
+                    <SeatItem
+                        key={sea.id}
+                        id={sea.id}
+                        isAvailable={sea.isAvailable}
+                        onClick={() => clickSeat(sea.id, sea.isAvailable)}
+                        selected={selectedSeat.includes(sea.id)}>
+                        {sea.name}
+
                     </SeatItem>
                 ))}
             </SeatsContainer>
-            
+
 
             <CaptionContainer>
                 <CaptionItem>
-                    <CaptionCircle isAvailable={1}/>
+                    <CaptionCircle isAvailable={1} />
                     Selecionado
                 </CaptionItem>
                 <CaptionItem>
-                    <CaptionCircle isAvailable={2}/>
+                    <CaptionCircle isAvailable={2} />
                     Disponível
                 </CaptionItem>
                 <CaptionItem>
-                    <CaptionCircle isAvailable={3}/>
+                    <CaptionCircle isAvailable={3} />
                     Indisponível
                 </CaptionItem>
             </CaptionContainer>
 
             <FormContainer>
-                Nome do Comprador:
-                <input placeholder="Digite seu nome..." />
+                <form onSubmit={addSeats}>
+                    Nome do Comprador:
+                    <input type="text" 
+                    value={nameId} 
+                    onChange={e => setName(e.target.value)} 
+                    placeholder="Digite seu nome..." />
 
-                CPF do Comprador:
-                <input placeholder="Digite seu CPF..." />
+                    CPF do Comprador:
+                    <input 
+                    value={cpf}
+                    onChange={e => setCpf(e.target.value)}
+                    placeholder="Digite seu CPF..." />
 
-                <button>Reservar Assento(s)</button>
+                    <button>Reservar Assento(s)</button>
+                </form>
             </FormContainer>
 
             <FooterContainer>
                 <div>
-                    <img src={seatsMovie.movie.posterURL} alt="poster" />
+                    <img src={movieSession.movie.posterURL} alt="poster" />
                 </div>
                 <div>
-                    <p>{seatsMovie.movie.title}</p>
-                    <p>{seatsMovie.day.weekday} - {seatsMovie.name}</p>
+                    <p>{movieSession.movie.title}</p>
+                    <p>{movieSession.day.weekday} - {movieSession.name}</p>
                 </div>
             </FooterContainer>
 
@@ -168,10 +204,8 @@ const CaptionItem = styled.div`
     font-size: 12px;
 `
 const SeatItem = styled.div`
-    border: ${props => props.isAvailable ? "1px solid #808F9D" : 
-"1px solid #F7C52B"};
-    background-color: ${props => props.isAvailable ? "#C3CFD9" : 
-"#FBE192"};
+    border: ${props => props.selected ? "1px solid #0E7D71" : props.isAvailable ? "1px solid #808F9D" : "1px solid #F7C52B"};
+    background-color: ${props => props.selected ? "#1AAE9E" : props.isAvailable ? "#C3CFD9" : "#FBE192"};
     height: 25px;
     width: 25px;
     border-radius: 25px;
